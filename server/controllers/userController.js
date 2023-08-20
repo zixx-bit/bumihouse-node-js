cdf294f67638bfb14ebdc47ebca3f63e533bdddc
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-21 00:55:53 +0300

    userRoute

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,6 +8,16 @@ export  const createUser = asyncHandler(async(req, res) => {
     let {email} = req.body;
 
     const userExists = await prisma.user.findUnique({where: {email: email}})
+    if(!userExists){
+        const user = await prisma.user.create({data: req.body});
+        res.send({
+            message: "user registered successfully",
+            user: user
+        });
+    }else res.status(201).send({message: 'User already registered'})
+    
+    
+
     console.log(email)
      
 });
