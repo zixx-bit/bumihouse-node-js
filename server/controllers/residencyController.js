4357cff61304dcb8a11a5fc27d53a68cb15c5124
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-22 00:36:47 +0300

    residency route

---
+++ server/controllers/residencyController.js
@@ -0,0 +1,34 @@
+import asyncHandler from "express-async-handler";
+
+import { prisma } from '../config/prismaConfig.js'
+
+export const createResidency = asyncHandler(async(req, res) => {
+    const {title, description, price, address, country, city, facilities, image, 
+        userEmail} = req.body.data;
+
+        console.log(req.body.data);
+
+        try {
+            const residency = await prisma.residency.
+            create({
+                data: {
+                title, 
+                description, 
+                price, 
+                address, 
+                country, city, 
+                facilities,
+                image,
+                owner : {connect :{email: userEmail}},
+                  },  
+                });
+                
+                res.send({ message: "Residency created successfully", residency})
+        } catch (error) {
+            if(error.code === "P2002")
+            {
+                throw new Error("A residency with the Address exists")
+            }
+            throw new Error(error.message)
+        }
+})
--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,4 +1,7 @@
 import  express from 'express';
+import { createResidency } from '../controllers/residencyController.js';
 const router = express.Router();
+
+router.post("/create", createResidency)
 
 export {router as residencyRoute}
