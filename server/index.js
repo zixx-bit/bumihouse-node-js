2a171185fd772d51fc2fb73a0c2a9fb3b29a24dc
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-21 23:23:55 +0300

    residency route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
 const app = express()
@@ -18,3 +19,4 @@ app.listen(PORT, () =>{
 })
 
 app.use('/api/user', userRoute)
+app.use('/api/residency', residencyRoute)
