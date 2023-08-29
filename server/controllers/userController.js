eba5d818d42d6ac82b2237fadb2f9d645036d7a5
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-29 23:23:46 +0300

    book visit controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,6 +23,22 @@ export const bookVisit = asyncHandler(async(req, res)=>{
     const {id} = req.params
 
     try {
+        const alreadyBooked = await prisma.user.findUnique({
+            where: {email: email}, 
+            select: {bookedVisits: true}
+        })
+
+        if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)){
+            res.status(400).json({message: "This residency is already booked by you"})
+        }else{
+            await prisma.user.update({
+                where: {email: email},
+                data : {
+                    bookedVisits: {push: {id: id}}
+                }
+            })
+        } 
+        res.send("Your visit is booked succesffully")
         
     } catch (err) {
         throw new Error(err.message)
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
  import express from 'express'
- import { createUser } from '../controllers/userController.js'
+ import {bookVisit, createUser } from '../controllers/userController.js'
 
  const router = express.Router();
 
