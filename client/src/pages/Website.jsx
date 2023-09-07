0cc6eba804a5d8dd7624ff40f5f7ea1f34bbd497
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-09-07 13:52:25 +0300

    client routes

--- client/src/pages/Website.jsx
+++ client/src/pages/Website.jsx
@@ -1,26 +1,32 @@
-import "./App.css";
-import Companies from "./components/Companies/Companies";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import GetStarted from "./components/GetStarted/GetStarted";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Residencies from "./components/Residencies
+// import "./App.css";
+import Companies from "../components/Companies/Companies";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+import GetStarted from "../components/GetStarted/GetStarted";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Residencies from "../components/Residencies/Residencies";
+import Value from "../components/Value/Value";
 
-return(
-<div className="App">
-<div>
-  <div className="white-gradient" />
-  <Header />
-  <Hero />
-</div>
-<Companies />
-<Residencies/>
-<Value/>
-<Contact/>
-<GetStarted/>
-<Footer/>
-</div>
-)
+
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div className="App">
+    <div>
+      <div className="white-gradient" />
+      <Header />
+      <Hero />
+    </div>
+    <Companies />
+    <Residencies/>
+    <Value/>
+    <Contact/>
+    <GetStarted/>
+    <Footer/>
+  </div>
+  )
+}
 
 export default Website
