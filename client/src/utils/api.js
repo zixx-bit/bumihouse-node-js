272cb338d32da60c2200e0f8519199e54841ed7d
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-09-27 10:11:49 +0300

    get all properties to properties page

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -9,6 +9,11 @@ export const api = axios.create({
 
 export const getAllProperties = async() =>{
     try {
+        const response = await api.get("/residency/allresd", {timeout: 10 * 1000, });
+        if (response.status === 400 || response.status === 500){
+            throw response.data
+        }
+        return response.data
         
     } catch (error) {
         toast.error("Something went wrong")
