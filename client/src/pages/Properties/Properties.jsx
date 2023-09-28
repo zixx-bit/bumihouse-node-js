60afc3393d31bf2f9a43bf51a3ce7ac5a1b5e83e
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-09-28 23:35:42 +0300

    yarn add loadash

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -2,24 +2,44 @@ import React from 'react'
 import Searchbar from '../../components/SearchBar/SearchBar'
 import './Properties.css'
 import useProperties from '../../hooks/useProperties'
+import {PuffLoader} from 'react-spinners'
+import PropertyCard from '../../components/PropertyCard/PropertyCard'
 
 
 const Properties = () => {
-  
+
   const {data, isError, isLoading} = useProperties()
   if (isError) {
     return(
       <div className='wrapper'>
       <span>Error while fetching data</span>
       </div>
+    )    
+  }
+
+  if (isLoading){
+    return(
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+        <PuffLoader
+          height = "80"
+          width = "80"
+          radius = {1}
+          color="#4066ff"
+          aria-label="puff-loading"
+        />
+      </div>
     )
-    
   }
 
   return (
     <div className='wrapper'>
     <div className='flexColCenter paddings innerWidth properties-container'>
       <Searchbar/>
+      <div className='paddings flexCenter properties'>
+        {
+          data.map((card, i)=> (<PropertyCard card={card} key={i}/>))
+        }
+      </div>
     </div>
     </div>
   )
