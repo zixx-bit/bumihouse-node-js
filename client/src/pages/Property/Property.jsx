b7940876b74e1bb3a0847981a426f63ae8283a1d
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-02 00:56:56 +0300

    Update Property.jsx

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 import {useQuery} from 'react-query';
 import {useLocation} from 'react-router-dom';
 import { getProperty } from '../../utils/api';
+import {PuffLoader} from "react-spinners";
 
 
 const Property = () => {
     const {pathname}=useLocation()
     const id =pathname.split('/').slice(-1)[0]
-    console.log(id);
+    // console.log(id);
     const{data, isLoading, isError}= useQuery(["resd", id], ()=> getProperty(id));
-    console.log(data)
+    // console.log(data)
+    if (isLoading) {
+        return(<div className='wrapper'>
+            <div className="flexCenter paddings">
+            <PuffLoader/>
+            </div>
+        </div>
+       )}
+
+    if (isError) 
+     {
+        return(
+            <div className='wrapper'>
+                <div className="flexCenter padding">
+                    <span>Error while fetching the property details</span>
+                </div>
+            </div>
+        )
+        
+    }   
+        
   return (
     <div>Property</div>
   )
