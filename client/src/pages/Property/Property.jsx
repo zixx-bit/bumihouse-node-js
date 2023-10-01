635b509ac821150ec8743da35473568c9a25a2a4
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-02 00:01:46 +0300

    Update Property.jsx

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {useQuery} from 'react-query';
+import {useLocation} from 'react-router-dom';
+
 
 const Property = () => {
+    const {pathname}=useLocation()
+    const id =pathname.split('/').slice(-1)[0]
+    console.log(id);
+    const{data, isLoading, isError}=useQuery(["resd"])
   return (
     <div>Property</div>
   )
