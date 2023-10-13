1d8994e37a0f48f8bb7d1583de5f3ebf0aefa802
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-13 13:36:46 +0300

    Update GeoCoderMarker.jsx

--- client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
+++ client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Marker, Popup } from 'react-leaflet'
+import React, { useState } from 'react'
+import { Marker, Popup, useMap } from 'react-leaflet'
 
-const GeoCoderMarker = () => {
+const GeoCoderMarker = ({address}) => {
+    
+    const map =useMap()
+    const [position, setPosition] = useState([60, 19])
   return (
     <Marker position={position} icon={DefaultIcon}>
            <Popup/>
