bb0c8c9ad0297727feac06f8bd1065411af69296
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-15 20:50:44 +0300

    Update GeoCoderMarker.jsx

--- client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
+++ client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react'
 import { Marker, Popup, useMap } from 'react-leaflet'
+import L from 'leaflet'
+import "leaflet/dist/leaflet.css"
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+    iconUrl:icon,
+    shadowUrl:iconShadow
+})
+L.Marker.prototype.options.icon =    DefaultIcon
 
 const GeoCoderMarker = ({address}) => {
 
