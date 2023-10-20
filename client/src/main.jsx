b187c4c314dde2b0031d92d08650a824dac1fee9
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-20 23:17:06 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import dotenv from '../../server/dotenv';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+  <Auth0Provider
+  domain = process.env.>
+    
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
