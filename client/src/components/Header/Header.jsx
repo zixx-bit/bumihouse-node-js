b76f5ff2137e7de5d167e234976409169e0e50e5
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-20 23:24:51 +0300

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,10 +5,12 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect } = useAuth0()
 
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
@@ -33,7 +35,7 @@ const Header = () => {
             <a href="mailto:zainkeepscode@gmail.com">Contact</a>
 
             {/* login button */}
-            <button className="button">
+            <button className="button" onClick={loginWithRedirect}>
               Login
             </button>
           </div>
