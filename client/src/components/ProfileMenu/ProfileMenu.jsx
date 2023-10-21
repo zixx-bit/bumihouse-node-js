51bb02d0fa119caaf6a8371c0fbd9097a7e288dd
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-10-22 02:21:43 +0300

    Update ProfileMenu.jsx

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import{Avatar, Menu} from '@mantine/core'
+import React from 'react';
+import{Avatar, Menu} from '@mantine/core';
 
 const ProfileMenu = ({user, logout}) => {
+
   return (
-    // <Menu>
-    //   <Menu.Target>
-    //     <Avatar src={user?.picture}  alt='user image' radius={"xl"}/>
-    //     </Menu.Target>    
-    //     <Menu.Dropdown>
-    //       <Menu.Item>
-    //         Favourites
-    //       </Menu.Item>
-    //     </Menu.Dropdown>
-    // </Menu>   
-    <div>
-      profile
-    </div>
+    <Menu>
+
+      <Menu.Target>
+        <Avatar src={user?.picture}  alt={user?.name} radius={"xl"}/>
+        </Menu.Target>  
+
+        <Menu.Dropdown>
+          <Menu.Item>
+            Favourites
+          </Menu.Item>
+        </Menu.Dropdown>
+        
+    </Menu>   
   )
+
 }
 
 export default ProfileMenu
