f2f3f66730b53c16e4fd01748f1b59043d51555a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-11-22 17:41:27 +0300

    Update userDetailsContext.js

--- client/src/context/userDetailsContext.js
+++ client/src/context/userDetailsContext.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const userDetailContext = createContext()
+const UserDetailContext = createContext()
 
-export default userDetailContext;
+export default UserDetailContext;
