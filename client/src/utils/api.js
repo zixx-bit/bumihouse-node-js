091543eb7c166273d2aa66027036f93b798dacab
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-11-26 22:07:27 +0300

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -28,7 +28,7 @@ export const getAllProperties = async() =>{
     }
 }
  
-export const getProperty = async( id) => {
+export const getProperty = async(id) => {
     try {
          const response = await api.get(`/residency/${id}`, {timeout: 10*1000},);
 
