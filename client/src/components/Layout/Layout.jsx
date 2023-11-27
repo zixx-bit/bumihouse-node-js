c671f30eb0635c7280d6f84ec0ef9f3f7a8805aa
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-11-27 17:00:44 +0300

    Update Layout.jsx

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -3,13 +3,13 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import userDetailContext from '../../context/userDetailsContext';
+import UserDetailsContext from '../../context/userDetailsContext';
 import { useMutation } from 'react-query';
 import { createUser } from '../../utils/api';
 
 const Layout = () => {
   const {isAuthethicated, user} = useAuth0()
-  const {setUserDetails} = useContext(userDetailContext)
+  const {setUserDetails} = useContext(UserDetailsContext)
 
   const {mutate} = useMutation({
     mutationKey: [user?.email],
