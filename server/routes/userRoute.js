60c25feafa0889059ef729072d43abb60fe7771b
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-11-27 20:27:27 +0300

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
  import express from 'express'
  import {allFavResidencies, bookVisit, cancelBooking, createUser, favResidency, getAllBooking } from '../controllers/userController.js'
+import jwtCheck from '../config/auth0Config.js';
 
  const router = express.Router();
 
-router.post("/register", createUser);
+router.post("/register", jwtCheck, createUser);
 router.post("/bookVisit/:id", bookVisit);
 router.get("/allBookings", getAllBooking)
 router.post("/removeBooking/:id", cancelBooking)
