81c361f96451ae00618eb7fe81ab3dd7fe22cc5a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-11-27 17:01:18 +0300

    Create auth0Config.js

---
+++ server/config/auth0Config.js
@@ -0,0 +1,10 @@
+import {auth} from 'express-oauth2-jwt-bearer'
+
+const jwtCheck = auth({
+    audience: "http://localhost:8000",
+    issuerBaseURL: "dev-8mti7gqox7uzcewe.us.auth0.com",
+    tokenSigningAlg: "RS256"
+    
+})
+
+export default jwtCheck
