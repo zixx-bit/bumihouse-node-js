153c511c5f6ce320676a663e42ca7adc0650f002
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-12-03 23:50:26 +0300

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
     audience: "http://localhost:8000",
-    issuerBaseURL: "dev-8mti7gqox7uzcewe.us.auth0.com",
+    issuerBaseURL: "https://dev-8mti7gqox7uzcewe.us.auth0.com",
     tokenSigningAlg: "RS256"
     
 })
