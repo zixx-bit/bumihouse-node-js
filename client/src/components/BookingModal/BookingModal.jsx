b134a159f251e67cd7e967c83414e0a2ea26dfb8
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-12-13 02:04:12 +0300

    Update BookingModal.jsx

--- client/src/components/BookingModal/BookingModal.jsx
+++ client/src/components/BookingModal/BookingModal.jsx
@@ -1,11 +1,16 @@
 import { Modal } from '@mantine/core'
 import React from 'react'
 
-const BookingModal = ({opened, setOpened, propertyId, email})
-
-const BookingModal = () => {
+const BookingModal = ({opened, setOpened, propertyId, email}) => {
   return (
-    <Modal>
+    <Modal
+    opened = {opened}
+    setOpened = {setOpened}
+    title="Select your date of visit"
+    centered>
+        <div>
+            <span>this is a booking modal</span>
+        </div>
 
     </Modal>
   )
