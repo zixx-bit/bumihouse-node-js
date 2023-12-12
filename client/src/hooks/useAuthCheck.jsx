e9bc2551a43aee4e8fc01a0610dac7e0d48f0d3a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-12-13 01:28:43 +0300

    Update useAuthCheck.jsx

--- client/src/hooks/useAuthCheck.jsx
+++ client/src/hooks/useAuthCheck.jsx
@@ -1,6 +1,17 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import toast from 'react-hot-toast'
 
 const useAuthCheck = () => {
+
+    const {isAuthenticated} = useAuth0()
+    const validateLogin = () =>{
+        if (!isAuthenticated) {
+            toast.error ("You must be loggged in")
+        }
+
+    }
+
   return (
     {
         validateLogin
