64c494e00d7752703006701ec214a3dedfb445a3
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-12-30 21:53:58 +0300

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,7 +4,7 @@ import jwtCheck from '../config/auth0Config.js';
 
  const router = express.Router();
 
-router.post("/register",jwtCheck, createUser);
+router.post("/register", createUser);
 router.post("/bookVisit/:id", bookVisit);
 router.get("/allBookings", getAllBooking)
 router.post("/removeBooking/:id", cancelBooking)
