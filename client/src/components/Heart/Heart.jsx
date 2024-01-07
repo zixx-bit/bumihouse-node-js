b7e51c3cd7f1fa9004dcaea842f29e40105ae81a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-07 22:14:47 +0300

    Create Heart.jsx

---
+++ client/src/components/Heart/Heart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AiFillHeart } from "react-icons/ai";
+
+
+const Heart = () => {
+  return (
+   <AiFillHeart size={24} color="white"/> 
+  )
+}
+
+export default Heart
