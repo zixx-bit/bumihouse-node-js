7b5b235529b98d612faedf835a49b9e0862805c7
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-08 22:00:18 +0300

    Update Heart.jsx

--- client/src/components/Heart/Heart.jsx
+++ client/src/components/Heart/Heart.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AiFillHeart } from "react-icons/ai";
+import useAuthCheck from '../../hooks/useAuthCheck';
+import { useMutation } from 'react-query';
+import { useAuth0 } from '@auth0/auth0-react';
+import UserDetailsContext from '../../context/UserDetailsContext';
+import { checkFavourites, updateFavourites } from '../../utils/common';
+import { toFav } from '../../utils/api';
 
 
-const Heart = () => {
-  return (
-   <AiFillHeart size={24} color="white"/> 
+const Heart = ({id}) => {
+    
+    const { validateLogin } = useAuthCheck()
+    const [heartColor, setHeartColor] = useState("white");
+    const {user}  = useAuth0 ()
+    const  {userDetails: { favourites, token}, setUserdetails} = useContext(UserDetailsContext)
+
+    useEffect(() =>{
+        setHeartColor(()=>{
+            checkFavourites(id, favourites)
+        }, [favourites])
+    })
+
+    const {mutate} = useMutation({ 
+        mutationFn: () => toFav(id, user?.email, token),
+        onSuccess: () => {
+            setUserdetails((prev)=> ({
+                ...prev, favourites: updateFavourites(id, prev.favourites) 
+            }
+            )) 
+        }
+    })
+
+    const handleLike = ()=>{
+        if (validateLogin) 
+        {   mutate()
+            setHeartColor((prev) => prev === "#fa3e5f"? "white": "#fa3e5f")    
+        }
+
+    }
+    return (
+   <AiFillHeart size={24} color={heartColor} onClick={(e) =>{
+        e.stopPropagation()
+        handleLike()
+   }}/> 
   )
 }
 
