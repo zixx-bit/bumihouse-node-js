cd0e5032af9e8e0247497df461378ffde29482b5
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-10 23:25:00 +0300

    Update common.js

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -33,6 +33,6 @@ export const updateFavourites = (id, favourites) =>{
   }
 } 
 
-export const checkFavourites = (id, favourites)=>{
-  return favourites.includes(id) ? "#fa3e5f" : "white";
+export const  checkFavourites = (id, favourites) => { 
+  // return favourites.includes(id) ? "#fa3e5f" : "white";
 }
