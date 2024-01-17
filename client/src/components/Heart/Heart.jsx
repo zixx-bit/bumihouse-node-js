7f4372fec5512488eac4331c66126cf8ac2d14ed
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-17 15:44:57 +0300

    Update Heart.jsx

--- client/src/components/Heart/Heart.jsx
+++ client/src/components/Heart/Heart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AiFillHeart } from "react-icons/ai";
+// import { AiFillHeart, AiTwotoneCar } from "react-icons/ai";
 import useAuthCheck from '../../hooks/useAuthCheck';
 import { useMutation } from 'react-query';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -11,38 +12,45 @@ import { toFav } from '../../utils/api';
 const Heart = ({id}) => {
     
     const { validateLogin } = useAuthCheck()
-    const [ heartColor, setHeartColor ] = useState("red");
+    const [heartColor, setHeartColor] = useState("white")
     const {user}  = useAuth0 ()
-    const  {userDetails: { favourites, token}, setUserdetails} = useContext(UserDetailsContext)
+    const  {
+        userDetails: { favourites, token},
+         setUserDetails } = useContext(UserDetailsContext)
 
-    useEffect(() =>{
-        setHeartColor(()=>{
-            checkFavourites(id, favourites)
-        }, [favourites])
-    })
+    // useEffect(() =>{
+    //     setHeartColor(()=>{
+    //         checkFavourites(id, favourites)
+    //     }, [favourites])
+    // })
 
     const {mutate, isLoading} = useMutation({ 
         mutationFn: () => toFav(id, user?.email, token),
         onSuccess: () => {
-            setUserdetails((prev)=> ({
+            setUserDetails((prev)=> ({
                 ...prev, favourites: updateFavourites(id, prev.favourites) 
             }
             )) 
         }
     })
 
-    const handleLike = ()=>{
+    const handleLike = () => {
+
         if (validateLogin() ) 
-        {   mutate()
-            setHeartColor((prev) => prev === "#fa3e5f"? "white": "#fa3e5f")    
+        {  
+             mutate()
+            setHeartColor((prev) => prev === "#fa3e5f" ? "white": "#fa3e5f")    
+
         }
 
     }
+
     return (
-   <AiFillHeart size={24} color={heartColor} onClick={(e) =>{
+   <AiFillHeart size={24} color={heartColor} onClick={(e) => {
         e.stopPropagation()
         handleLike()
-   }} /> 
+     }
+   } /> 
   )
 }
 
