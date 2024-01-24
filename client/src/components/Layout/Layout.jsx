0e3bf9d99710ae23138191c70edc075989be1d85
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-24 21:18:57 +0300

    Update Layout.jsx

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -6,8 +6,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 import UserDetailsContext from "../../context/UserDetailsContext";
 import { useMutation } from "react-query";
 import { createUser } from "../../utils/api";
+import useFavourites from "../../hooks/useFavourites";
 
 const Layout = () => {
+
+  useFavourites() 
+  
   const { isAuthenticated, user, getAccessTokenWithPopup } = useAuth0();
   const { setUserDetails } = useContext(UserDetailsContext);
 
