5b0b52d6cc336527884158aeec9b1670bab326b1
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-26 00:10:22 +0300

    Update useFavourites.jsx

--- client/src/hooks/useFavourites.jsx
+++ client/src/hooks/useFavourites.jsx
@@ -1,31 +1,33 @@
-import React, { useContext, useRef, useEffect } from 'react'
-import UserDetailsContext from '../context/UserDetailsContext'
-import { useAuth0 } from '@auth0/auth0-react'
-import { useQuery } from 'react-query'
-import { getAllFav } from '../utils/api'
+import React, { useContext, useRef, useEffect } from 'react';
+import UserDetailsContext from '../context/UserDetailsContext';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useQuery } from 'react-query';
+import { getAllFav } from '../utils/api';
 
 const useFavourites = () => {
-  const {userDetails, setUserDetails} = useContext(UserDetailsContext)
-  const {user} = useAuth0()
-  const queryRef = useRef()
+  const {userDetails, setUserDetails} = useContext(UserDetailsContext);
+  const {user} = useAuth0();
+  const queryRef = useRef();
 
   const {data, isError, isLoading, refetch} = useQuery({
     queryKey: "allFavResidencies",
     queryFn: () => getAllFav(user?.email, userDetails?.token),
     onSuccess: (data) =>
-      setUserDetails((prev) =>({...prev, favourites:data})),
+      setUserDetails((prev) => ({...prev, favourites:data})),
       enabled: user !== undefined,
       staleTime: 30000,
-    })
+    });
+
+    // console.log(setUserDetails?.prev)
 
     queryRef.current = refetch;
 
     useEffect(() =>{
-      queryRef.current && queryRef.current()
+      queryRef.current && queryRef.current();
     },[ userDetails?.token])
     
   
   return {data, isError, isLoading, refetch};
-}
+};
 
 export default useFavourites
