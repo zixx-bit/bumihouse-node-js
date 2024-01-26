6813bf9be53b043480a6a8998e4ea39980051635
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-01-26 23:14:18 +0300

    Update common.js

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -24,14 +24,22 @@ export const sliderSettings = {
 
 };
 
-export const updateFavourites = (id, favourites) => { 
-  if ( favourites.includes(id)) 
-  {
-   return  favourites.filter((resId) => resId!==id)  
-  }else{
-    return[...favourites, id]
+// export const updateFavourites = (id, favourites) => { 
+//   if ( favourites?.includes(id)) 
+//   {
+//    return  favourites.filter((resId) => resId!==id)  
+//   }else{
+//     return[...favourites, id]
+//   }
+// };
+export const updateFavourites = (id, favourites) => {
+  if (Array.isArray(favourites) && favourites.includes(id)) {
+    return favourites.filter((resId) => resId !== id);
+  } else {
+    return [...(Array.isArray(favourites) ? favourites : []), id];
   }
 };
+
 
 // export const  checkFavourites = (id, favourites) => { 
 //   return  favourites?.includes(id) ? "#fa3e5f" : "white";
