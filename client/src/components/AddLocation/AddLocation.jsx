58dab1451525fbd2468195edc783454334ae5bb8
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-02-02 00:10:26 +0300

    Update AddLocation.jsx

--- client/src/components/AddLocation/AddLocation.jsx
+++ client/src/components/AddLocation/AddLocation.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useForm } from '@mantine/form';
 import { validateString } from '../../utils/common';
-import { Select, TextInput } from '@mantine/core';
+import { Button, Group, Select, TextInput } from '@mantine/core';
 import useCountries from '../../hooks/useCountries'
 import Map from '../Map/Map'
 // import map from '../../../node_modules'
 
 
 
-const  AddLocation = ({propertyDetails, setPropertyDetails}) => {
+const  AddLocation = ({propertyDetails, setPropertyDetails, nextStep}) => {
   const {getAll} = useCountries()
    
   const form = useForm({
@@ -26,24 +26,42 @@ const  AddLocation = ({propertyDetails, setPropertyDetails}) => {
   })
 
   const {country, city, address} = form.values;
+
+  const handleSubmit = () =>{
+    const{hasErrors} = form.validate()
+    if (!hasErrors) {
+      setPropertyDetails((prev) => ({...prev, city, address, country}))
+      nextStep()
+    }
+  }
   
   return (    
-        <form>
-        {/* left side */}
-          <div className="flexCenter">
-
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit()
+        }}>
+       
+          <div className="flexCenter" style={{
+            justifyContent: " space-between",
+            gap: "3rem",
+            marginTop: "3rem",
+            flexDirection: "row"
+          }}>
+          
+          {/* left side */}
           {/* inputs */} 
           <div className="flexColStart">
           <Select
           w={"100%"}
           withAsterisk
-          label="country"
+          label="Country"
           clearable
           searchable
           data={getAll()}
           {
             ...form.getInputProps("country", {type: "input"})
           }>
+          
           </Select>
 
           <TextInput
@@ -65,16 +83,22 @@ const  AddLocation = ({propertyDetails, setPropertyDetails}) => {
           </TextInput>
 
             </div>
-          </div>
+          
 
           {/* right side */}
-          <div>
+          <div style={{flex:1}}>
           <Map 
             address = {address}
             city = {city}
             country = {country}
           />
           </div>
+          </div>
+
+          <Group position="center" mt={"xl"}>
+            <Button type="submit">Next Step</Button>
+          </Group>
+
         </form>
   )
 }
