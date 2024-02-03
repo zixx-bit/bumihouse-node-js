03ce0bd0205f5cba498db4e7b5ae067299d995b9
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-02-04 02:01:36 +0300

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -174,5 +174,24 @@ export const getAllBookings = async(email, token ) => {
     }
 }
 
+export const createResidency = async (data, token) => {
+    try {
+        const res = await api.post(
+            `/residency/create`,
+            {
+                data
+            },
+            {
+                headers: {
+                    Authorization : `Bearer ${token}`
+                },
+            }
+        )
+        
+    } catch (error) {
+        throw error
+    }
+}
+
 
 
