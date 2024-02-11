d704baf9b09cf202f5f3c7fdb0803ed14c738518
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-02-11 23:57:11 +0300

    Update SearchBar.jsx

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { HiLocationMarker } from "react-icons/hi";
 
 
-const SearchBar = () => {
+const SearchBar = ({filter, setFilter}) => {
   return (
     <div className="flexCenter search-bar">
     <HiLocationMarker color="var(--blue)" size={25} />
-    <input type="text" />
+    <input type="text" 
+    placeholder='Search by title/city/country...' 
+    value={filter} 
+    onChange={(e) => setFilter(e.target.value)} />
     <button className="button">Search</button>
   </div>
 
