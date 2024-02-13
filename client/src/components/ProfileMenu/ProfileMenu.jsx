75727e4d5fb0c394759febea61df455ac48a2eaf
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-02-14 00:00:04 +0300

    Update ProfileMenu.jsx

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import { ColorSwatch,
   Group,
   useMantineTheme,
@@ -17,6 +17,7 @@ import { ColorSwatch,
   Navbar,
   Header,
   Burger,
+  UnstyledButton,
   Indicator,
   Avatar}  from '@mantine/core';
 
@@ -24,107 +25,112 @@ import { ColorSwatch,
     IconSettings,
     IconMessageCircle,
     IconPhoto,
+    IconCalendar,
+    IconHeartFilled,
     IconSearch,
+    IconBook,
     IconArrowsLeftRight,
     IconTrash,
     IconLogout,
-    IconLogout2
+    IconLogout2,
+    IconCalendarCheck,
+    IconChevronRight
   } from "@tabler/icons-react";
-  import "./ProfileMenu.css";
-  import { LocalStorageCache } from '@auth0/auth0-react';
+  // import "./ProfileMenu.css";
+  import { useNavigate } from 'react-router-dom';
 
 
-  //  const ProfileMenu = (user, logout)=> {
-  //   const theme = useMantineTheme();
+
+
+   const ProfileMenu = ({user, logout})=> {
+    const navigate = useNavigate()
+
+    return (
+      <Box className="ProfileMenu">
+          <div style={{ display: "flex", alignItems: "center", height: "100%" }}>  
+            <Menu
+              shadow="md"
+              width={200}
+              position={"top-end"}
+              returnFocus
+              closeOnEscape transitionProps={{ transition: 'rotate-left', duration: 150 }}
+            >
+              <Menu.Target className="flexStart"   style={{
+        padding: 'var(--mantine-spacing-md)',
+        color: 'var(--mantine-color-text)',
+        borderRadius: 'var(--mantine-radius-sm)',
+      }}>       
+                <Group>
+                {/* <Indicator></Indicator> */}
+                  <Avatar src= {user?.picture} alt={user?.email}
+                    radius="xl"
+                   />
+                   <div style={{ flex:1 }}>
+                    <Text size="sm" fw={500}> { user?.name } </Text>
+                    <Text c="dimmed" size="xs"> {user?.email} </Text>
+                   </div>
+                   {<IconChevronRight size="1rem" />}
+              
+                </Group>
+
+              </Menu.Target>
   
-  //   // return (
-  //   //   <Box className="ProfileMenu">
-  //   //     {/* <Header height={{ base: 50, md: 70 }} p="md"> */}
-  //   //       <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-  //   //         {/* <Burger size="sm" color={theme.colors.gray[6]} mr="xl" /> */}
+              <Menu.Dropdown>
+                <Menu.Label>Application</Menu.Label>
+                <Menu.Item icon={<IconHeartFilled size={16}  />} onClick={()=> navigate ("./favourites", {replace: true})} >Favourites</Menu.Item>
+                <Menu.Item icon={<IconCalendarCheck size={16} />} onClick={() => navigate("./bookings", {replace: true})}>
+                  Bookings
+                </Menu.Item>
+                {/* <Menu.Item
+                  icon={<IconSearch size={14} />}              
+                >
+                  Search
+                </Menu.Item> */}
   
-  //   //         {/* <Text>Application header</Text> */}
-  //   //         <Menu
-  //   //           shadow="md"
-  //   //           width={200}
-  //   //           // position={"top-end"}
-  //   //           returnFocus
-  //   //           closeOnEscape
-  //   //         >
-  //   //           <Menu.Target>
-  //   //             <Indicator>
-  //   //               <Avatar
-  //   //                 radius="xl"
-  //   //                 src= {user?.image} alt={user?.email} />
-  //   //             </Indicator>
-  //   //           </Menu.Target>
+                <Menu.Divider />
   
-  //   //           <Menu.Dropdown>
-  //   //             <Menu.Label>Application</Menu.Label>
-  //   //             <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-  //   //             <Menu.Item icon={<IconMessageCircle size={14} />}>
-  //   //               Messages
-  //   //             </Menu.Item>
-  //   //             <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
-  //   //             <Menu.Item
-  //   //               icon={<IconSearch size={14} />}
-  //   //               rightSection={
-  //   //                 <Text size="xs" color="dimmed">
-  //   //                   ⌘K
-  //   //                 </Text>
-  //   //               }
-  //   //             >
-  //   //               Search
-  //   //             </Menu.Item>
-  
-  //   //             <Menu.Divider />
-  
-  //   //             <Menu.Label>Danger zone</Menu.Label>
-  //   //             <Menu.Item icon={<IconArrowsLeftRight size={14} />}>
-  //   //               Transfer my data
-  //   //             </Menu.Item>
-  //   //             <Menu.Item  color="red" 
-  //   //             onClick={() => {
-  //   //               localStorage.clear();
-  //   //               logout()
-  //   //             }} >
-  //   //               Logout
-  //   //             </Menu.Item>
-  //   //           </Menu.Dropdown>
-  //   //         </Menu>
-  //   //       </div>
-  //   //     {/* </Header> */}
-  //   //      {/* <Box mt={220}>Some block</Box> */}
-  //   //    </Box>
-  //   // )
-  // }
+                <Menu.Label>Account</Menu.Label>
+                 <Menu.Item  color="red" icon={<IconLogout2 size={16}/>}
+                  onClick={() => {
+                  localStorage.clear();
+                  logout()
+                }} >
+                  Logout
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </div>
+       </Box>
+    )
+  }
 
   
 
-const ProfileMenu = ({user, logout}) => {
-  return (
-     <Menu>
-      <Menu.Target>
-        <Avatar src={user?.picture} alt={user?.email}  radius={"xl"}/>
-        </Menu.Target>  
-        <Menu.Dropdown>
-          <Menu.Item>
-            Favourites 
-          </Menu.Item>
+// const ProfileMenu = ({user, logout}) => {
+//   const navigate = useNavigate()
+//   return (
+//      <Menu>
+//       <Menu.Target>
+//         <Avatar src={user?.picture} alt={user?.email}  radius={"xl"}/>
+//         </Menu.Target>  
+//         <Menu.Dropdown>
+//           <Menu.Item onClick={()=> navigate("./favourites", {replace:true})}>
+//             Favourites 
+//           </Menu.Item>
          
-          <Menu.Item>
-            Bookings
-          </Menu.Item>
+//           <Menu.Item onClick={() => navigate("./bookings", {replace: true})}>
+//             Bookings
+//           </Menu.Item>
          
-          <Menu.Item onClick={()=>{
-            localStorage.clear();
-            logout()
-          }}>Log out
+//           <Menu.Item onClick={()=>{
+//             localStorage.clear();
+//             logout()
+//           }}>Log out
 
-          </Menu.Item>
-        </Menu.Dropdown>     
-     </Menu>   
-  )
-}
+//           </Menu.Item>
+//         </Menu.Dropdown>     
+//      </Menu>   
+//   )
+// }
 
 export default ProfileMenu
